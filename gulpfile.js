57d986e38d55bac83c26bbc45d58157418dfd5ec
Andriy Khimich <andrykhimich@gmail.com.noreply.github.com>
2021-02-25 15:05:47 +0200

    done

--- gulpfile.js
+++ gulpfile.js
@@ -78,11 +78,11 @@ gulp.task("js", function () {
     .src([
       // js libs uncomment what you need
       "node_modules/jquery/dist/jquery.min.js",
-      // "node_modules/slick-carousel/slick/slick.min.js",
-      "node_modules/svg4everybody/dist/svg4everybody.min.js",
+      "node_modules/slick-carousel/slick/slick.min.js",
+      // "node_modules/svg4everybody/dist/svg4everybody.min.js",
       // "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js",
       // "node_modules/jquery-nice-select/js/jquery.nice-select.min.js",
-                  // "src/libs/datepicker/dist/js/datepicker-full.min.js",
+      // "src/libs/datepicker/dist/js/datepicker-full.min.js",
       "src/js/script.js",
     ])
     .pipe(sourcemaps.init())
